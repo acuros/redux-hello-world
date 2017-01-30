ca34246b78100ec94d14759a10c40471a778a63d
Joshua <seungyeon@joshua.kim>
2017-01-30 11:48:35 +0900

    Separate app directory

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 module.exports = {
     context: __dirname,
     entry: {
-        app: './app.js'
+        app: './app/app.js'
     },
     output: {
         path: 'output/generated/',
